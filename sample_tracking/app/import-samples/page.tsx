f2852a24bd41c4a3bc8ef40e7ad8f3b00bb90fe2
j-e-shaw <jeshaw@google.com>
2023-07-26 14:14:19 -0600

    Fix a couple copy/paste issues with the visibility selectors.

--- sample_tracking/app/import-samples/page.tsx
+++ sample_tracking/app/import-samples/page.tsx
@@ -224,7 +224,7 @@ export default function ImportCsv() {
                     <option value="unknown">Unkown</option>
                 </select>
                 <label htmlFor="sampleVisibility">Sample visibility</label>
-                    <select className="form-select" id="sampleVisibility" aria-label="Select sample trusted status">
+                    <select className="form-select" id="sampleVisibility" aria-label="Select sample visibility">
                         <option value="public">Publicly available</option>
                         <option value="logged_in">Available to any logged-in user</option>
                         <option value="organization">Available to my organization only</option>
