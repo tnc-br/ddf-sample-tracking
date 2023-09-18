360f973660b25ab042ee21d9210efad9187f9983
Kazob1998 <kazob@google.com>
2023-09-18 15:22:55 -0400

    create git prd and set env

--- sample_tracking/app/firebase_config.tsx
+++ sample_tracking/app/firebase_config.tsx
@@ -1,8 +1,9 @@
+//TODO replace it by config
 export const firebaseConfig = {
   apiKey: "AIzaSyCgLUyR-rGuT2qbHgPsJ8l0mG_u6S7keHg",
-  authDomain: "river-sky-386919.firebaseapp.com",
-  projectId: "river-sky-386919",
-  storageBucket: "river-sky-386919.appspot.com",
+  authDomain: "timberid-prd.firebaseapp.com",
+  projectId: "timberid-prd",
+  storageBucket: "timberid-prd.appspot.com",
   messagingSenderId: "843836318122",
   appId: "1:843836318122:web:856d513c850325a32b8bd3"
 };
